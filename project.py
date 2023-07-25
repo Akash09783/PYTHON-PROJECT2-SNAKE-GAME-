3592c2a83aaaffb06f7e2e035a362afc8a76fd5f
Akash <kumawat.aakash515@gmail.com>
2023-07-25 21:08:07 +0530

    second commit

---
+++ project.py
@@ -0,0 +1,24 @@
+import pygame
+import time
+import random
+snake_speed = 15
+
+window_x = 720
+window_y = 480
+
+
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
+
+pygame.init()
+
+pygame.display.set_caption('GeeksforGeeks Snakes')
+game_window = pygame.display.set_mode((window_x, window_y))
+
+fps = pygame.time.Clock()
+
+
+snake_position = [100, 50]
